d8d1113b167cd795a74dc72b647b4d0f0e559eef
Qianxiong Xu <20086454@mail.wit.ie>
2020-01-03 14:31:52 +0000

    Configure router guarder

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '@/store/store'
 import FrontIndex from '@/components/front/index'
 import BackIndex from '@/components/back/index'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   linkActiveClass: 'active',
   mode: 'history',
   routes: [
@@ -21,3 +22,35 @@ export default new Router({
     }
   ]
 })
+
+// Router guarder
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((r) => r.meta.requireAuth)) {
+    if (store.state.token) {
+      next()
+    } else {
+      if (localStorage.token && localStorage.getItem('account')) {
+        store.state.token = localStorage.token
+        store.state.account = JSON.parse(localStorage.getItem('account'))
+        next()
+      } else {
+        let path = to.path
+        if (path.indexOf('admin') >= 0) {
+          next({
+            path: '/admin/login',
+            query: {redirect: to.fullPath}
+          })
+        } else {
+          next({
+            path: '/login',
+            query: {redirect: to.fullPath}
+          })
+        }
+      }
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
