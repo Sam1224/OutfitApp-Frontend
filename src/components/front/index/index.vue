49149ae36578c203efa36a1f39e477af104d746a
Qianxiong Xu <20086454@mail.wit.ie>
2020-04-11 14:45:50 +0100

    Add v-footer

--- src/components/front/index/index.vue
+++ src/components/front/index/index.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="index">
     <router-view></router-view>
+    <div class="footer-wrapper">
+      <v-footer></v-footer>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import VFooter from '@/components/front/v-footer/v-footer'
+
+  export default {
+    components: {
+      VFooter
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .footer-wrapper
+    position: fixed
+    left: 0
+    bottom: -1px
+    width: 100%
+    box-shadow: 0 0 10px 0 hsla(0, 6%, 58%, 0.6)
+    height: 50px
+    z-index: 999
+    background: lightgreen
 </style>
---
+++ src/components/front/v-footer/v-footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="footer">
+    <router-link class="footer-item" to="/home">
+      <i class="iconfont el-icon-user-solid"></i>
+    </router-link>
+    <router-link class="footer-item" to="/vton">
+      <i class="iconfont el-icon-s-goods"></i>
+    </router-link>
+    <router-link class="footer-item" to="/retrieval">
+      <i class="iconfont el-icon-s-goods"></i>
+    </router-link>
+    <router-link class="footer-item" to="/personal">
+      <i class="iconfont el-icon-s-custom"></i>
+    </router-link>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'VFooter'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .footer
+    display: flex
+    width: 100%
+    height: 50px
+    background: #fff
+    .footer-item
+      flex: 1
+      display: flex
+      justify-content: center
+      align-items: center
+      flex-direction: column
+      color: #333
+      font-size: 12px
+      .iconfont
+        margin-bottom: 4px
+        font-size: 18px
+    .router-link-active
+      color: dodgerblue
+</style>
