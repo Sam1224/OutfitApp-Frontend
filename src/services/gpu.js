1371910744c2c4ba09c725d5b25cc5926d434d15
Qianxiong Xu <20086454@mail.wit.ie>
2020-04-29 11:30:15 +0100

    Adapt to the process of using https in aws instance

--- src/services/gpu.js
+++ src/services/gpu.js
@@ -3,10 +3,15 @@
  * @Date: 2020/4/7
  * @Version: 1.0
  **/
-const awsUrl = '34.192.9.145'
+// const awsUrl = '34.192.9.145'
+// const awsApis = {
+//   'vton': `http://${awsUrl}:5000/api_server`,
+//   'retrieval': `http://${awsUrl}:5012/mmfashion`
+// }
+const awsUrl = 'xusam2412.ml'
 const awsApis = {
-  'vton': `http://${awsUrl}:5000/api_server`,
-  'retrieval': `http://${awsUrl}:5012/mmfashion`
+  'vton': `https://${awsUrl}/api_server`,
+  'retrieval': `https://${awsUrl}/mmfashion`
 }
 
 module.exports = awsApis
